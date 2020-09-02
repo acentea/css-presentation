cbfab7349135b4a99846e1502c0a666dad66fa8f
CENTEA Andrei <Andrei.CENTEA@frequentis.com>
2020-09-02 20:45:13 +0300

    Create routes for flexBox page and cssGrid page

--- css-presentation-project/src/router/index.ts
+++ css-presentation-project/src/router/index.ts
@@ -1,27 +1,32 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter, {RouteConfig} from 'vue-router'
 import Home from '../views/Home.vue'
+import CssGrid from '../views/CssGrid.vue'
+import FlexBox from '../views/FlexBox.vue'
 
 Vue.use(VueRouter)
 
-  const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes: Array<RouteConfig> = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/flexbox',
+        name: 'FlexBox',
+        component: FlexBox
+    },
+
+    {
+        path: '/cssgrid',
+        name: 'CssGrid',
+        component: CssGrid
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
