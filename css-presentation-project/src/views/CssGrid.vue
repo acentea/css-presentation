010582c9628d83c5b096ca4313739baf5417c1a1
CENTEA Andrei <Andrei.CENTEA@frequentis.com>
2020-09-10 14:53:04 +0300

    Css grid template with grid-areas : desktop and mobile

--- css-presentation-project/src/views/CssGrid.vue
+++ css-presentation-project/src/views/CssGrid.vue
@@ -1,23 +1,19 @@
 <template>
     <body>
     <div id="content">
-        <div class="one">1</div>
-        <div class="two">2</div>
-        <div class="three">3</div>
-        <div class="nested">
-            <p>1</p>
-            <p>2</p>
-            <p>3</p>
-            <p>4</p>
-        </div>
-        <div class="five">5</div>
-        <div class="six">6</div>
+        <header>Header</header>
+        <main>Main</main>
+        <section>Section</section>
+        <aside>Aside</aside>
+        <nav>Nav</nav>
+        <footer>footer</footer>
     </div>
     </body>
 </template>
 
 <style scoped>
 
+    /* mobile grid*/
     #content {
         color: #fff;
         font-family: 'Nunito Semibold';
@@ -26,51 +22,69 @@
         margin: 0 auto;
 
         display: grid;
-        grid-template-columns: repeat(6, 1fr);
-        grid-template-rows: repeat(4, minmax(150px, auto));
-        grid-gap: 20px;
+        grid-template-columns: repeat(4, 1fr);
+        grid-auto-rows: minmax(100px, auto);
+
+        grid-gap: 10px;
+
+        grid-template-areas:
+                "abcd abcd abcd abcd"
+                "footer footer footer footer"
+                "main main main main"
+                "main main main main"
+                "aside aside nav nav"
+                "section section section section"
+                "section section section section";
 
     }
 
-    .one {
-        grid-column: 1 / 3;
+    /* desktop grid*/
+    @media screen and (min-width: 760px) {
+        #content {
+            color: #fff;
+            font-family: 'Nunito Semibold';
+            text-align: center;
+            max-width: 1500px;
+            margin: 0 auto;
+
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            grid-auto-rows: minmax(100px, auto);
+            grid-gap: 10px;
+
+            grid-template-areas: "abcd abcd abcd ." "aside aside main main" "nav nav main main" "section section section ." "section section section ." "footer footer footer footer";
+        }
     }
 
-    .two {
-        grid-column: 3 / 7;
-    }
-
-    .three {
-        grid-column: 1 / 5;
-        grid-row: 2 / 4;
-    }
-
-    .nested {
-        grid-column: 4 / 7;
-        grid-row: 2 / 4;
-
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-    }
-
-    .five {
-        grid-column: 3 / 7;
-    }
-
-    .six {
-        grid-column: 1 / 3;
-        grid-row: 4;
-    }
-
-    #content div {
-        background: #3bbced;
+    #content > * {
+        background: aqua;
         padding: 30px;
     }
 
-    #content div:nth-child(even) {
-        background: #777;
-        padding: 30px;
+    header {
+        grid-area : abcd;
     }
+
+    main {
+        grid-area : main;
+    }
+
+    section {
+        grid-area : section;
+    }
+
+    aside {
+        grid-area : aside;
+    }
+
+    nav {
+        grid-area : nav;
+    }
+
+    footer {
+        grid-area : footer;
+    }
+
 
 </style>
 
